f7d6d446cbdba739ee463d00134884cfe73b4f27
Mar Mustafa <amustaf@email.arizona.edu>
2020-07-12 17:32:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     element_index = 0 
     while element_index < src[row_index].length do
       if src[row_index][element_index] == String 
-        final_string.join()
+        final_string.join(src[row_index][element_index])
       end
       element_index +=1 
     end
